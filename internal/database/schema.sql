46029bad23f19c959302e090b5b64f74221b388f
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-11-23 08:30:10 -0700

    relational-ify db schema

--- internal/database/schema.sql
+++ internal/database/schema.sql
@@ -1,13 +1,31 @@
 create database if not exists visa_sponsors_db;
 use visa_sponsors_db;
 
-drop table if exists companies;
-create table companies (
-    org_id int auto_increment,
-    org_name varchar(255) not null,
-    city varchar(255),
-    county varchar(255),
-    job_type_rating varchar(255),
-    visa_route varchar(255),
-    primary key (org_id)
+drop table if exists organisation_job;
+drop table if exists organisation;
+drop table if exists job;
+
+CREATE TABLE `organisation` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255),
+  `city` varchar(255),
+  `county` varchar(255)
 );
+
+CREATE TABLE `job` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `type` varchar(255),
+  `rating` varchar(255),
+  `visa_route` varchar(255)
+);
+
+CREATE TABLE `organisation_job` (
+  `organisation_id` integer,
+  `job_id` integer,
+  PRIMARY KEY (`organisation_id`, `job_id`)
+);
+
+ALTER TABLE `organisation_job` ADD FOREIGN KEY (`organisation_id`) REFERENCES `organisation` (`id`);
+
+ALTER TABLE `organisation_job` ADD FOREIGN KEY (`job_id`) REFERENCES `job` (`id`);
+
