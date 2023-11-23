2659ae7dc7cfff8861ceb6a195f43394d1c1c16a
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-11-23 09:12:12 -0700

    implement queue structure for database generation

--- internal/database/seed.go
+++ internal/database/seed.go
@@ -1,20 +1,65 @@
 package database
 
 import (
-	"database/sql"
 	"encoding/csv"
+	"fmt"
 	"log"
 	"os"
 )
 
+type OrgNode struct {
+	Val  *Organisation
+	Next *OrgNode
+}
+
+type OrgQueue struct {
+	Len  int
+	Head *OrgNode
+	Tail *OrgNode
+}
+
+func New() *OrgQueue {
+	return &OrgQueue{
+		Len:  0,
+		Head: nil,
+		Tail: nil,
+	}
+}
+
+func (q *OrgQueue) Push(org *Organisation) {
+	node := &OrgNode{Val: org, Next: nil}
+	q.Len++
+	if q.Len <= 1 {
+		q.Head = node
+		q.Tail = node
+		return
+	}
+	q.Head.Next = node
+	q.Head = node
+	return
+}
+
+func (q *OrgQueue) Pop() (*Organisation, error) {
+	if q.Len < 1 {
+		return nil, fmt.Errorf("no items to pop from queue")
+	}
+	q.Len--
+	result := q.Tail
+	q.Tail = result.Next
+	result.Next = nil
+	return result.Val, nil
+}
+
+func (q *OrgQueue) Peek() (*Organisation, error) {
+	if q.Len < 1 {
+		return nil, fmt.Errorf("no items to peek on queue")
+	}
+	return q.Tail.Val, nil
+}
+
 func SeedDB() {
 	db := MustConnectDB()
 	defer db.Close()
-
-	// err := initTable(db)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
 
 	file, err := os.Open("internal/database/data.csv")
 	if err != nil {
@@ -25,62 +70,11 @@ func SeedDB() {
 		log.Fatal("error reading file: ", err)
 	}
 
-	for i, row := range data {
-		if i == 0 {
-			continue
-		}
-		err := addCompany(
-			db,
-			row[0],
-			row[1],
-			row[2],
-			row[3],
-			row[4],
-		)
-		if err != nil {
-			log.Fatal("error writing new row: ", err)
-		}
-	}
-}
+	// "Organisation Name","Town/City","County","Type & Rating","Route"
+	// 0 -> Organisation.Name
+	// 1 -> Organisation.City
+	// 2 -> Organisation.County
+	// 3 (SPLIT)-> Job.Type, Job.Rating
+	// 4 -> Job.VisaRoute
 
-func addCompany(db *sql.DB, org_name string, city string, county string, job_type_rating string, visa_route string) error {
-	result, err := db.Exec(
-		"INSERT INTO companies (org_name, city, county, job_type_rating, visa_route) VALUES (?, ?, ?, ?, ?)",
-		org_name, city, county, job_type_rating, visa_route,
-	)
-	if err != nil {
-		return err
-	}
-
-	rows, _ := result.RowsAffected()
-
-	log.Printf("addCompany completed affecting %d row(s)", rows)
-	return err
-}
-
-func initTable(db *sql.DB) error {
-	_, err := db.Exec("DROP TABLE IF EXISTS companies")
-	if err != nil {
-		return err
-	}
-
-	log.Println("Cleared DB")
-
-	_, err = db.Exec(`
-		CREATE TABLE companies (
-			org_id int AUTO INCREMENT,
-			org_name varchar(255) NOT NULL,
-			city varchar(255),
-			county varchar(255),
-			job_type_rating varchar(255),
-			primary key (org_id)
-		)
-	`)
-	if err != nil {
-		return err
-	}
-
-	log.Println("Initialized DB")
-
-	return nil
 }
